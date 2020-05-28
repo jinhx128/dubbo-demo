c40e3f07cb50a398b83b811a521149cc89c0869f
luoyusoft <luoyusoft@126.com>
2020-05-29 00:18:47 +0800

    1、删除没用的文件

--- script-file/sh/dubbo-demo-service-restart-6051.sh
+++
@@ -1,3 +0,0 @@
-ps -fu root|grep /usr/local/service/service/6051/dubbo-demo-service-1.0.0.jar|grep -v grep|awk '{print $2}'|xargs kill -9
-sleep 2 
-java -jar /usr/local/service/service/6051/dubbo-demo-service-1.0.0.jar --spring.profiles.active=prod-6051 >> /usr/local/service/tomcat/dubbo-demo-service/6051/tomcat.log 2>&1   &
--- script-file/sh/dubbo-demo-web-restart-6061+8441.sh
+++
@@ -1,3 +0,0 @@
-ps -fu root|grep /usr/local/service/web/6061+8441/dubbo-demo-web-1.0.0.jar|grep -v grep|awk '{print $2}'|xargs kill -9
-sleep 2 
-java -jar /usr/local/service/web/6061+8441/dubbo-demo-web-1.0.0.jar --spring.profiles.active=prod-6061 >> /usr/local/service/tomcat/dubbo-demo-web/6061+8441/tomcat.log 2>&1   &
